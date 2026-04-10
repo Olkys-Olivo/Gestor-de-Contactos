96ed23107b68d70426087d250aa4ec6fc8dd994a
Pedro Angel Sanchez Luciano <pedroangelsanchez56@gmail.com>
2026-04-09 21:25:13 -0400

    Add files via upload

--- GestordeContactos/Repositories/ContacRepository.cs
+++ GestordeContactos/Repositories/ContacRepository.cs
@@ -1,36 +1,76 @@
-using Contaccase.Models;
-using Contaccase.Database;
+using Contactocase.Models;
+using Contactocase.Database;
 
-class ContacRepository(AppDbContext context) {
+class ContactoRepository(AppDbContext context)
+{
   private readonly AppDbContext _context = context;
 
-  public List<ContacModel> SelectAll() {
-    return _context.Contacts.ToList();
+  public List<ContactoModel> SelectAll()
+  {
+    return _context.Contactos.ToList();
   }
 
-  public int Delete(int ID_Contacto) {
-    var contac = _context.Contacts.Find(ID_Contacto);
+  public int Delete(int ID_Contacto)
+  {
+    var contacto = _context.Contactos.Find(ID_Contacto);
 
-    if (contac is null) return 0;
-    _context.Contacts.Remove(contac);
+    if (contacto is null) return 0;
+
+    _context.Contactos.Remove(contacto);
     return _context.SaveChanges();
   }
 
-  public int Insert(string title, string year, string isbn) {
-    var book = new BookModel { Title = title, Year = year, Isbn = isbn };
-    _context.Books.Add(book);
+  public int Insert(string Nombre, string Apellido, string? Empresa, string Telefono, string? Puesto, string? Gmail, bool Es_Favorito, string? Nota)
+  {
+    var contacto = new ContactoModel
+    {
+      Nombre = Nombre,
+      Apellido = Apellido,
+      Empresa = Empresa,
+      Telefono = Telefono,
+      Puesto = Puesto,
+      Gmail = Gmail,
+      Es_Favorito = Es_Favorito,
+      Nota = Nota
+    };
+
+    _context.Contactos.Add(contacto);
     return _context.SaveChanges();
   }
 
-  public int Update(string title, string year, string isbn, int id) {
-    var book = _context.Books.Find(id);
+  public int Update(string Nombre, string Apellido, string? Empresa, string Telefono, string? Puesto, string? Gmail, bool Es_Favorito, string Fecha_Creacion, string? Nota, int ID_Contacto)
+  {
+    var contacto = _context.Contactos.Find(ID_Contacto);
 
-    if (book is null) return 0;
+    if (contacto is null) return 0;
 
-    book.Title = title;
-    book.Year = year;
-    book.Isbn = isbn;
+    contacto.Nombre = Nombre;
+    contacto.Apellido = Apellido;
+    contacto.Empresa = Empresa;
+    contacto.Telefono = Telefono;
+    contacto.Puesto = Puesto;
+    contacto.Gmail = Gmail;
+    contacto.Es_Favorito = Es_Favorito;
+    contacto.Fecha_Creacion = Fecha_Creacion;
+    contacto.Nota = Nota;
 
     return _context.SaveChanges();
+  }
+   public List<ContactoModel> Buscarnombre(string Nombre) {
+    return _context.Contactos
+      .Where(c => c.Nombre.Contains(Nombre))
+      .ToList();
+  }
+
+  public List<ContactoModel> GetFavoritos() {
+    return _context.Contactos
+      .Where(c => c.Es_Favorito)
+      .ToList();
+  }
+
+  public List<ContactoModel> GetNoFavoritos() {
+    return _context.Contactos
+      .Where(c => !c.Es_Favorito)
+      .ToList();
   }
 }
